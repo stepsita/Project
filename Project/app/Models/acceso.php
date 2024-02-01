5dd451e3eb665cc1ab1927555e69456f3d19ce87
Gaby <gabymitsan@gmail.com>
2024-02-01 02:41:16 -0400

    acceso

---
+++ Project/app/Models/acceso.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class acceso extends Model
+{
+    use HasFactory;
+}
